23b25c891ef5e461991c1816553ecc849964f1c3
Dashiell Boyles <93666673+Dash1704@users.noreply.github.com>
2023-01-03 08:34:10 +0000

    Extracted form logic

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,12 +1,15 @@
-import React, { useContext } from 'react'
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
+import BlogPostForm from '../components/BlogPostForm'
 
 const EditScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Edit Screen - {navigation.getParam('id')}</Text>
-    </View>
-  )
+  const { state } = useContext(Context)
+
+  const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'))
+
+  return <BlogPostForm />
+  
 }
 
 const styles = StyleSheet.create({})
